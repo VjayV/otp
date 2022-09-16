3febdd5974d5201c0d957a979f57e23d64937fb4
vjayv <v95jay@gmail.com>
2022-09-16 18:15:40 +0530

    first commit

---
+++ src/services/otp.service.js
@@ -0,0 +1,15 @@
+const { User } = require('../models');
+
+/**
+ * Get user by id
+ * @param {ObjectId} id
+ * @returns {Promise<User>}
+ */
+const getUserById = async (id) => {
+  return User.findById(id);
+};
+
+
+module.exports = {
+  getUserById
+};
